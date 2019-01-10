0fea2de28b21ca558e214f3441b2fdfb9ce1e577
giscafer <giscafer@outlook.com>
2019-01-10 22:53:21 +0800

    fix(encode): single  geometry export ECharts got undefined

--- encode.js
+++ encode.js
@@ -6,7 +6,7 @@
  * @date    2016-06-04T01:48:33+0800
  * 参考：https://github.com/ecomfe/echarts/blob/8eeb7e5abe207d0536c62ce1f4ddecc6adfdf85e/src/util/mapData/rawData/encode.js
  */
-!(function(name, definition) {
+!(function (name, definition) {
     var hasDefine = typeof define === 'funciton',
         hasExports = typeof module !== 'undefined' && module.exports;
     if (hasDefine) {
@@ -18,42 +18,65 @@
     } else {
         this[name] = definition();
     }
-})('Encoder', function() {
-    function Encoder(){}
+})('Encoder', function () {
+    function Encoder() { }
 
-    Encoder.prototype.convert2Echarts=function(rawStr,fileName,type){
-        var results="";
+    Encoder.prototype.convert2Echarts = function (rawStr, fileName, type) {
+        var results = "";
         var json = JSON.parse(rawStr);
         // Meta tag
         json.UTF8Encoding = true;
         var features = json.features;
-        if (!features) {
-            return;
-        }
-        features.forEach(function (feature){
-            var encodeOffsets = feature.geometry.encodeOffsets = [];
-            var coordinates = feature.geometry.coordinates;
-            if (feature.geometry.type === 'Polygon') {
-                coordinates.forEach(function (coordinate, idx){
-                    coordinates[idx] = encodePolygon(
-                        coordinate, encodeOffsets[idx] = []
-                    );
-                });
-            } else if(feature.geometry.type === 'MultiPolygon') {
-                coordinates.forEach(function (polygon, idx1){
-                    encodeOffsets[idx1] = [];
-                    polygon.forEach(function (coordinate, idx2) {
-                        coordinates[idx1][idx2] = encodePolygon(
-                            coordinate, encodeOffsets[idx1][idx2] = []
+        // console.log(json);
+        if (features) {
+            features.forEach(function (feature) {
+                var encodeOffsets = feature.geometry.encodeOffsets = [];
+                var coordinates = feature.geometry.coordinates;
+                if (feature.geometry.type === 'Polygon') {
+                    coordinates.forEach(function (coordinate, idx) {
+                        coordinates[idx] = encodePolygon(
+                            coordinate, encodeOffsets[idx] = []
                         );
                     });
-                });
-            }
-        });
-        if(type==='json'){
-            results=JSON.stringify(json);
-        }else{
-            results=addEchartsJsWrapper(JSON.stringify(json),fileName);
+                } else if (feature.geometry.type === 'MultiPolygon') {
+                    coordinates.forEach(function (polygon, idx1) {
+                        encodeOffsets[idx1] = [];
+                        polygon.forEach(function (coordinate, idx2) {
+                            coordinates[idx1][idx2] = encodePolygon(
+                                coordinate, encodeOffsets[idx1][idx2] = []
+                            );
+                        });
+                    });
+                }
+            });
+        } else {
+            var geometries = json.geometries;
+            geometries.forEach(function (geometry) {
+                var encodeOffsets = geometry.encodeOffsets = [];
+                var coordinates = geometry.coordinates;
+                if (geometry.type === 'Polygon') {
+                    coordinates.forEach(function (coordinate, idx) {
+                        coordinates[idx] = encodePolygon(
+                            coordinate, encodeOffsets[idx] = []
+                        );
+                    });
+                } else if (geometry.type === 'MultiPolygon') {
+                    coordinates.forEach(function (polygon, idx1) {
+                        encodeOffsets[idx1] = [];
+                        polygon.forEach(function (coordinate, idx2) {
+                            coordinates[idx1][idx2] = encodePolygon(
+                                coordinate, encodeOffsets[idx1][idx2] = []
+                            );
+                        });
+                    });
+                }
+            });
+        }
+
+        if (type === 'json') {
+            results = JSON.stringify(json);
+        } else {
+            results = addEchartsJsWrapper(JSON.stringify(json), fileName);
         }
         return results;
     };
@@ -68,8 +91,8 @@
 
         for (var i = 0; i < coordinate.length; i++) {
             var point = coordinate[i];
-            result+=encode(point[0], prevX);
-            result+=encode(point[1], prevY);
+            result += encode(point[0], prevX);
+            result += encode(point[1], prevY);
 
             prevX = quantize(point[0]);
             prevY = quantize(point[1]);
@@ -80,52 +103,52 @@
 
     function addAMDWrapper(jsonStr) {
         return ['define(function() {',
-                    '    return ' + jsonStr + ';',
-                '});'].join('\n');
+            '    return ' + jsonStr + ';',
+            '});'].join('\n');
     }
 
-    function addEchartsJsWrapper(jsonStr,fileName) {
+    function addEchartsJsWrapper(jsonStr, fileName) {
         return ['(function (root, factory) {',
-                    "   if (typeof define === 'function' && define.amd) {",
-                    "       define(['exports', 'echarts'], factory);",
-                    "   } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {",
-                    "       factory(exports, require('echarts'));",
-                    "   } else {",
-                    "       factory({}, root.echarts);",
-                    "   }",
-                    " }(this, function (exports, echarts) {",
-                    "       var log = function (msg) {",
-                    "           if (typeof console !== 'undefined') {",
-                    "               console && console.error && console.error(msg);",
-                    "           }",
-                    "       }",
-                    " if (!echarts) {",
-                    "       log('ECharts is not Loaded');",
-                    "           return;",
-                    "       }",
-                    " if (!echarts.registerMap) {",
-                    "       log('ECharts Map is not loaded')",
-                    "       return;",
-                    " }",
-                    "  echarts.registerMap('"+fileName+"',"+ jsonStr,
-                '  )}));'].join('\n');
+            "   if (typeof define === 'function' && define.amd) {",
+            "       define(['exports', 'echarts'], factory);",
+            "   } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {",
+            "       factory(exports, require('echarts'));",
+            "   } else {",
+            "       factory({}, root.echarts);",
+            "   }",
+            " }(this, function (exports, echarts) {",
+            "       var log = function (msg) {",
+            "           if (typeof console !== 'undefined') {",
+            "               console && console.error && console.error(msg);",
+            "           }",
+            "       }",
+            " if (!echarts) {",
+            "       log('ECharts is not Loaded');",
+            "           return;",
+            "       }",
+            " if (!echarts.registerMap) {",
+            "       log('ECharts Map is not loaded')",
+            "       return;",
+            " }",
+            "  echarts.registerMap('" + fileName + "'," + jsonStr,
+            '  )}));'].join('\n');
     }
 
-    function encode(val, prev){
+    function encode(val, prev) {
         // Quantization
         val = quantize(val);
         // var tmp = val;
         // Delta
         val = val - prev;
 
-        if (((val << 1) ^ (val >> 15)) + 64 === 8232) { 
+        if (((val << 1) ^ (val >> 15)) + 64 === 8232) {
             //WTF, 8232 will get syntax error in js code
             val--;
         }
         // ZigZag
         val = (val << 1) ^ (val >> 15);
         // add offset and get unicode
-        return String.fromCharCode(val+64);
+        return String.fromCharCode(val + 64);
         // var tmp = {'tmp' : str};
         // try{
         //     eval("(" + JSON.stringify(tmp) + ")");
